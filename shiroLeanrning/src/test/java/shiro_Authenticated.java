bd6ec91b16a7690abdfbe259fe097b08a1e3d66c
奋进的安大人 <17010052734@163.com>
2018-06-04 23:51:50 +0800

    shiro 认证、登录的学习

---
+++ shiroLeanrning/src/test/java/shiro_Authenticated.java
@@ -0,0 +1,46 @@
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.mgt.DefaultSecurityManager;
+import org.apache.shiro.realm.SimpleAccountRealm;
+import org.apache.shiro.subject.Subject;
+import org.junit.Before;
+import org.junit.Test;
+
+//认证
+public class shiro_Authenticated {
+
+    SimpleAccountRealm simpleAccountRealm = new SimpleAccountRealm();
+
+    @Before
+    public void addUser(){
+        //模拟从数据库中查找用户放在realm中
+        simpleAccountRealm.addAccount("FlightingYan","123456");
+    }
+
+    @Test
+    public void testAuthentication(){
+
+        //1创建SecurityManager
+        DefaultSecurityManager defultSecurityManager = new DefaultSecurityManager();
+
+        //将SecurityManager放在容器中
+        SecurityUtils.setSecurityManager(defultSecurityManager);
+
+        //将Realm放在SecurityManager中
+        defultSecurityManager.setRealm(simpleAccountRealm);
+
+        //获取传来的subject
+        Subject subject = SecurityUtils.getSubject();
+
+        UsernamePasswordToken usernamePasswordToken = new UsernamePasswordToken("FlightingYan","123456");
+        //主体提交认证请求——>SecurityManager会去realm拿对应的数据对比认证
+        subject.login(usernamePasswordToken);
+
+        System.out.println("isAuthenticated:"+subject.isAuthenticated());
+
+        subject.logout();
+
+        System.out.println("isAuthenticated:"+subject.isAuthenticated());
+
+    }
+}
---
+++ shiroLeanrning/src/test/java/shiro_Authorizer.java
@@ -0,0 +1,43 @@
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.mgt.DefaultSecurityManager;
+import org.apache.shiro.realm.SimpleAccountRealm;
+import org.apache.shiro.subject.Subject;
+import org.junit.Before;
+import org.junit.Test;
+
+
+//授权
+public class shiro_Authorizer {
+    SimpleAccountRealm simpleAccountRealm = new SimpleAccountRealm();
+
+    @Before
+    public void addUser(){
+        //设置角色
+        simpleAccountRealm.addAccount("FlightingYan","123456","admin","user");
+    }
+
+    @Test
+    public void shiroAuthorizer(){
+
+        //创建SecurityManager
+        DefaultSecurityManager defaultSecurityManager = new DefaultSecurityManager();
+
+        //容器中放置defaultSecurityManager
+        SecurityUtils.setSecurityManager(defaultSecurityManager);
+        //将Realm放在SecurityManager中
+        defaultSecurityManager.setRealm(simpleAccountRealm);
+        //用户名密码token
+        UsernamePasswordToken token = new UsernamePasswordToken("FlightingYan","123456");
+        //从SecurityUtils中获取主体subject
+        Subject subject = SecurityUtils.getSubject();
+        //主体提交认证请求——>SecurityManager会去realm拿对应的数据对比认证
+        subject.login(token);
+        //判断是否 为认证用户
+        System.out.println("isAuthenticated:"+subject.isAuthenticated());
+
+        //检查角色
+        subject.checkRoles("admin","user");
+
+    }
+}
